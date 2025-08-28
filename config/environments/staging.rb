f9b484c884d2012e834b37e2670108dd478f582d
MaFai <6803441+mafai@users.noreply.github.com>
2025-08-28 15:25:38 +0800

    init

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -54,6 +54,9 @@ Rails.application.configure do
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
+  # Configure SolidCable for staging
+  config.solid_cable.connects_to = { database: { writing: :cable } }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
